e47a939762263585d01a2d5a1011856f1c485fc3
David Xiang <xiangdongwei@pku.edu.cn>
2019-12-08 01:49:40 +0800

    [frontend] products part

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header style="font-size: 40px; color: #820010">北大有鱼，其名为闲。</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu>
+            <el-menu-item :index="'index'">
+              <template slot="title"><i class="el-icon-menu"></i>首页</template>
+            </el-menu-item>
+            <el-menu-item :index="'analysis'">
+              <template slot="title"><i class="el-icon-menu"></i>分析</template>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-container>
+          <el-main style="margin-left: 10px;" v-loading="isLoading">
+            <el-row>
+              <el-col :span="6" v-for="(o, index) in 20" :key="o" >
+                <el-card shadow="hover" body-style="padding: 0px" style="height: 360px; margin: 10px;">
+                  <el-image style="width: 100%; height: 220px" v-bind:src="products[index%3].productInfo.imgsrc" fit="cover"/>  
+                  <div style="margin-left: 10px; margin-right: 10px">
+                    <div align="left">
+                      <div style="display:inline; color: red; font-size: 14px">¥</div>
+                      <div style="display:inline; color: red; font-size: 22px">{{products[index%3].productInfo.price.toFixed(2)}}</div>
+                    </div>
+                    <div class="title" align="left" style="font-color: #F56C6C; font-size: 14px">{{products[index%3].productInfo.title}}</div>
+                    <div style="margin: 5px"/>
+                    <el-popover
+                      placement="top-start"
+                      width="500px"
+                      trigger="hover">
+                      <el-table height="200px" :data="products[index%3].comments.slice(0,Math.min(5,products[index%3].comments.length))">
+                        <el-table-column width="100" property="buyerName" show-overflow-tooltip label="姓名"></el-table-column>
+                        <el-table-column width="70" property="time" label="日期"></el-table-column>
+                        <el-table-column width="330" property="content" show-overflow-tooltip="true" label="评论"></el-table-column>
+                      </el-table>
+                      <el-badge v-bind:value="products[index%3].comments.length" style="margin: 7px" slot="reference">
+                        <el-button size="medium" icon="el-icon-chat-dot-square">评论</el-button>
+                      </el-badge>
+                    </el-popover>
+                    <el-button size="medium" style="margin: 5px">加购<i class="el-icon-shopping-cart-1 el-icon--right"></i></el-button>
+                  </div>
+                </el-card>
+              </el-col>
+            </el-row>
+          </el-main>
+        </el-container>
+      </el-container>
+      <el-footer style="color: black; font-size: 12px; margin: 20px">
+        版权所有©北京大学数据库概论课程 | 地址：北京市海淀区颐和园路5号第二教学楼316 | 邮编：100871 | xdw@pku.edu.cn | 京ICP备05065075号-1 | 京公网安备 110402430047 号
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Login from './components/Login.vue'
+import mock_products from './assets/mock_products.js'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      isLogin: false,
+      isLoading: false,
+      products: mock_products.products,
+      commentData: []
+    }
+  }, 
+  created(){
+  
+    // this.$axios({
+    //   method: 'GET',
+    //   url: 'http://localhost/mock/login',
+    //   }).then((res)=>{
+    //   window.console.log(res)
+    //   this.xml = res.data;
+    // })
+
+  },
+  methods: {
+    // comment(i){
+    //   product.i = 0;
+    // }
   }
 }
 </script>
@@ -23,6 +94,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.title{
+      width: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis; /* 文本溢出时显示省略号来代表被修剪的文本 */
+      display: -webkit-box; /* 必须结合的属性 ，将对象作为弹性伸缩盒子模型显示 */
+      -webkit-box-orient: vertical; /* 必须结合的属性 ，设置或检索伸缩盒对象的子元素的排列方式 */
+      -webkit-line-clamp: 2; /* 文本需要显示多少行 */
 }
 </style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container style="height:100%; width:100%">
+    <LoginBox/>
+  </el-container>
+</template>
+
+<script>
+import LoginBox from "./LoginBox.vue";
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  },
+  components: {
+    LoginBox
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
