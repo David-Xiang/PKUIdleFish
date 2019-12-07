035b7fa2910814548475a1aa3b6bfdeb2da8e46b
Jack <48796459+cyclekiller@users.noreply.github.com>
2019-12-07 21:00:42 +0800

    Update database_structure.sql
    账号从1000开始auto increment

--- database_structure.sql
+++ database_structure.sql
@@ -1,142 +1,51 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- 主机： 127.0.0.1
--- 生成日期： 2019-12-07 06:42:28
--- 服务器版本： 10.4.8-MariaDB
--- PHP 版本： 7.3.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- 数据库： `pkuidlefish`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `account`
---
-
-
--- TODO: account的id为自动分配，从四位数开始
 -- TODO: name的约束：不可重复，不可以空格开头或结尾，及字数限制
-CREATE TABLE `account` (
-  `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `passwd` varchar(16) NOT NULL,
-  `birth` int(11) NOT NULL,
-  `sex` char(1) NOT NULL,
-  `email` varchar(32) NOT NULL,
-  `phone` bigint(20) NOT NULL,
-  `isseller` tinyint(4) NOT NULL,
-  `deleted` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    passwd VARCHAR(16) NOT NULL,
+    birth DATE NOT NULL,
+    sex CHAR(1) NOT NULL,
+    email VARCHAR(32) NOT NULL,
+    phone BIGINT NOT NULL,
+    isseller TINYINT NOT NULL,
+    deleted TINYINT NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci AUTO_INCREMENT = 1000;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS cart;
+CREATE TABLE cart(
+    buyerid INT UNSIGNED NOT NULL,
+    productid INT UNSIGNED NOT NULL
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
 
---
--- 表的结构 `cart`
---
+DROP TABLE IF EXISTS category;
+CREATE TABLE category(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    name VARCHAR(16) NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
 
-CREATE TABLE `cart` (
-  `buyerid` int(11) NOT NULL,
-  `productid` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS comment;
+CREATE TABLE comment(
+    buyerid INT UNSIGNED NOT NULL,
+    productid INT UNSIGNED NOT NULL,
+    time DATETIME NOT NULL,
+    content TEXT NOT NULL
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- 表的结构 `category`
---
-
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `comment`
---
-
-CREATE TABLE `comment` (
-  `buyerid` int(11) NOT NULL,
-  `productid` int(11) NOT NULL,
-  `datetime` varchar(16) NOT NULL,
-  `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- 表的结构 `product`
---
-
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `categoryid` tinyint(4) NOT NULL,
-  `title` varchar(128) NOT NULL,
-  `imgsrc` varchar(256) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `sellerid` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `saletime` bigint(12) NOT NULL,
-  `sold` tinyint(4) NOT NULL,
-  `visible` tinyint(4) NOT NULL,
-  `deleted` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- 转储表的索引
---
-
---
--- 表的索引 `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`);
-
---
--- 表的索引 `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- 表的索引 `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- 使用表AUTO_INCREMENT `product`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS product;
+CREATE TABLE IF NOT EXISTS product(
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+    categoryid TINYINT NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    imgsrc VARCHAR(256) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    sellerid INT UNSIGNED NOT NULL,
+    description TINYTEXT NOT NULL,
+    saletime DATETIME NOT NULL,
+    sold TINYINT NOT NULL,
+    visible TINYINT NOT NULL,
+    deleted TINYINT NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
