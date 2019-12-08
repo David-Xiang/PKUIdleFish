24f2a750a467a7d6544e38ebfdf5002c9d95b6fa
Jack <48796459+cyclekiller@users.noreply.github.com>
2019-12-08 13:38:49 +0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,11 @@
 -- 查询格式: login?username=&password=
--- 返回值: username_not_exist, incorrect_password, user_type
--- username_not_exist: 0 or 1
+-- 返回值: incorrect_password, user_type
 -- incorrect_password: 0 or 1
 -- user_type: in ['buyer','forseller','seller','admin','deleted']
+-- 注意: 账号不存在时返回为空！
 
-SELECT IF(tmp.passwd = NULL, 1, 0), IF(tmp.passwd = '123456', 0, 1), tmp.type as user_type
-FROM
-	(SELECT a.passwd, a.type FROM account AS a WHERE a.name = 'abc') AS tmp;
+SELECT IF(tmp.en_passwd = myhash(?), 0, 1), tmp.type as user_type
+FROM (SELECT a.en_passwd, a.type FROM account AS a WHERE a.name = ?) AS tmp
+
+-- 第一个问号填密码
+-- 第二个问号填昵称
