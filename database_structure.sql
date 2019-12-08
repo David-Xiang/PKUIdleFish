209ea1cd59942d2001aef92f4aea7d185fa6ef0a
Jack <48796459+cyclekiller@users.noreply.github.com>
2019-12-08 08:44:17 +0800

    use enum to represent state

--- database_structure.sql
+++ database_structure.sql
@@ -1,5 +1,4 @@
 -- TODO: name的约束：不可重复，不可以空格开头或结尾，及字数限制
--- TODO: 将magicint变为枚举类型，增加可读性
 DROP TABLE IF EXISTS account;
 CREATE TABLE account(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -9,7 +8,7 @@ CREATE TABLE account(
     sex CHAR(1) NOT NULL,
     email VARCHAR(32) NOT NULL,
     phone BIGINT NOT NULL,
-    magicint TINYINT NOT NULL
+    state ENUM('buyer','forseller','seller','admin','deleted') NOT NULL DEFAULT 'buyer'
 ) ENGINE = InnoDB COLLATE = utf8mb4_general_ci AUTO_INCREMENT = 1000;
 
 DROP TABLE IF EXISTS cart;
@@ -42,5 +41,5 @@ CREATE TABLE IF NOT EXISTS product(
     sellerid INT UNSIGNED NOT NULL,
     description TINYTEXT NOT NULL,
     saletime DATETIME NOT NULL,
-    magicint TINYINT NOT NULL
+    state ENUM('draft','sale','sold','returned','deleted') NOT NULL DEFAULT 'draft'
 ) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
