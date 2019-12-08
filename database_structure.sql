49328ff3757ee769a973d11a3e9798634702ebe7
Jack <48796459+cyclekiller@users.noreply.github.com>
2019-12-08 09:14:44 +0800

    add foreign key constraints

--- database_structure.sql
+++ database_structure.sql
@@ -1,45 +1,59 @@
--- TODO: name的约束：不可重复，不可以空格开头或结尾，及字数限制
-DROP TABLE IF EXISTS account;
+-- TODO: name的约束：不可以空格开头或结尾，及字数限制
+DROP DATABASE IF EXISTS pkuidlefish;
+CREATE DATABASE pkuidlefish;
+USE pkuidlefish;
+
 CREATE TABLE account(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(32) NOT NULL UNIQUE,
     passwd VARCHAR(16) NOT NULL,
     birth DATE NOT NULL,
     sex CHAR(1) NOT NULL,
     email VARCHAR(32) NOT NULL,
     phone BIGINT NOT NULL,
-    state ENUM('buyer','forseller','seller','admin','deleted') NOT NULL DEFAULT 'buyer'
+    state ENUM('buyer','forseller','seller','admin','deleted') NOT NULL DEFAULT 'buyer',
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB COLLATE = utf8mb4_general_ci AUTO_INCREMENT = 1000;
 
-DROP TABLE IF EXISTS cart;
-CREATE TABLE cart(
-    buyerid INT UNSIGNED NOT NULL,
-    productid INT UNSIGNED NOT NULL
-) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
-
-DROP TABLE IF EXISTS category;
 CREATE TABLE category(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(16) NOT NULL
+    id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(16) NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
 
-DROP TABLE IF EXISTS comment;
-CREATE TABLE comment(
-    buyerid INT UNSIGNED NOT NULL,
-    productid INT UNSIGNED NOT NULL,
-    time DATETIME NOT NULL,
-    content TEXT NOT NULL
-) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
-
-DROP TABLE IF EXISTS product;
 CREATE TABLE IF NOT EXISTS product(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    categoryid TINYINT NOT NULL,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    categoryid TINYINT UNSIGNED NOT NULL,
     title VARCHAR(128) NOT NULL,
     imgsrc VARCHAR(256) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     sellerid INT UNSIGNED NOT NULL,
     description TINYTEXT NOT NULL,
     saletime DATETIME NOT NULL,
-    state ENUM('draft','sale','sold','returned','deleted') NOT NULL DEFAULT 'draft'
+    state ENUM('draft','sale','sold','returned','deleted') NOT NULL DEFAULT 'draft',
+    PRIMARY KEY (id),
+    CONSTRAINT fk_product_categoryid FOREIGN KEY (categoryid)
+    REFERENCES category(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_product_sellerid FOREIGN KEY (sellerid)
+    REFERENCES account(id) ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE cart(
+    buyerid INT UNSIGNED NOT NULL,
+    productid INT UNSIGNED NOT NULL,
+    CONSTRAINT fk_cart_buyerid FOREIGN KEY (buyerid)
+    REFERENCES account(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_cart_productid FOREIGN KEY (productid)
+    REFERENCES product(id) ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE comment(
+    buyerid INT UNSIGNED NOT NULL,
+    productid INT UNSIGNED NOT NULL,
+    time DATETIME NOT NULL,
+    content TEXT NOT NULL,
+    CONSTRAINT fk_comment_buyerid FOREIGN KEY (productid)
+    REFERENCES account(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_comment_productid FOREIGN KEY (productid)
+    REFERENCES product(id) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE = InnoDB COLLATE = utf8mb4_general_ci;
