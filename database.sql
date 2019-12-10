a99a9e77d666c740430c70326949652484a8d959
Jack <cyclekiller@pku.edu.cn>
2019-12-10 19:22:14 +0800

    combine 'requesttime' and 'approvetime' into 'updatetime'
    updatetime表示账号注册/请求/成为卖家/被封时间

--- database.sql
+++ database.sql
@@ -18,10 +18,11 @@ CREATE TABLE account(
     sex CHAR(1),
     email VARCHAR(32),
     phone BIGINT,
-    type ENUM('buyer','forseller','seller','admin','deleted') DEFAULT 'buyer'
+    type ENUM('buyer','forseller','seller','admin','deleted') DEFAULT 'buyer',
+    updatetime DATE
 ) AUTO_INCREMENT = 1000;
 
-CREATE TABLE IF NOT EXISTS product(
+CREATE TABLE product(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     categoryid TINYINT UNSIGNED,
     title VARCHAR(128),
@@ -36,16 +37,6 @@ CREATE TABLE IF NOT EXISTS product(
 CREATE TABLE category(
     categoryid TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     categoryname VARCHAR(16)
-);
-
-CREATE TABLE forseller(
-    forsellerid INT UNSIGNED,
-    requesttime DATETIME
-);
-
-CREATE TABLE seller(
-    sellerid INT UNSIGNED,
-    approvetime DATETIME
 );
 
 CREATE TABLE cart(
@@ -79,20 +70,13 @@ RETURN SHA2(CONCAT('sha2', original, 'pkuidlefish2019'), 512);
 -- Data
 -- 
 
-INSERT INTO account(username, en_passwd, birth, sex, email, phone, type) VALUES
-('user1', myhash('passwd1'), '2000-01-01', 'M', 'user1@pku.edu.cn', '10086000001', 'forseller'),
-('user2', myhash('passwd2'), '2000-01-02', 'F', 'user2@pku.edu.cn', '10086000002', 'seller'),
-('user3', myhash('passwd3'), '2000-01-03', 'M', 'user3@pku.edu.cn', '10086000003', 'forseller'),
-('user5', myhash('passwd5'), '2000-01-04', 'F', 'user4@pku.edu.cn', '10086000004', 'seller'),
-('user6', myhash('passwd6'), '2000-01-05', 'M', 'user5@pku.edu.cn', '10086000005', 'buyer'),
-('user7', myhash('passwd7'), '2000-01-06', 'M', 'user6@pku.edu.cn', '10086000006', 'buyer');
-
-INSERT INTO forseller(forsellerid, requesttime) VALUES
-('1000', '2010-10-10 10:10:10'),
-('1002', '2010-10-11 10:10:10');
-INSERT INTO seller(sellerid, approvetime) VALUES
-('1001', '2009-09-09 09:09:09'),
-('1003', '2009-09-08 09:09:09');
+INSERT INTO account(username, en_passwd, birth, sex, email, phone, type, updatetime) VALUES
+('user1', myhash('passwd1'), '2000-01-01', 'M', 'user1@pku.edu.cn', '10086000001', 'forseller', '2011-11-11'),
+('user2', myhash('passwd2'), '2000-01-02', 'F', 'user2@pku.edu.cn', '10086000002', 'seller', 2010-10-10'),
+('user3', myhash('passwd3'), '2000-01-03', 'M', 'user3@pku.edu.cn', '10086000003', 'forseller', '2009-09-09'),
+('user5', myhash('passwd5'), '2000-01-04', 'F', 'user4@pku.edu.cn', '10086000004', 'seller', '2008-08-08'),
+('user6', myhash('passwd6'), '2000-01-05', 'M', 'user5@pku.edu.cn', '10086000005', 'buyer', '2006-06-06'),
+('user7', myhash('passwd7'), '2000-01-06', 'M', 'user6@pku.edu.cn', '10086000006', 'buyer', '2007-07-07');
 
 INSERT INTO category (categoryid, categoryname) VALUES
 ('1', 'category1'),
