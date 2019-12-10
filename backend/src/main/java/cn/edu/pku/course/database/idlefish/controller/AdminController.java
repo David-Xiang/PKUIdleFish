fbd1e1ad5aa0c8b1337c87f0fe1583281c16e1b4
Jack <cyclekiller@pku.edu.cn>
2019-12-10 13:11:49 +0800

    add response to /admin/user
    先按照用户类型排序，同类型的按照申请审核/审核通过/注册时间从新到旧排序

---
+++ backend/src/main/java/cn/edu/pku/course/database/idlefish/controller/AdminController.java
@@ -0,0 +1,42 @@
+package cn.edu.pku.course.database.idlefish.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.edu.pku.course.database.idlefish.repository.UserRepository;
+
+@RestController
+@RequestMapping("admin")
+public class AdminController {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("test")
+	public String test() {
+		return "it works!";
+	}
+
+	private static String[] TYPE = { "admin", "forseller", "seller", "deleted", "buyer" };
+	private static String[] ORDER_BY = { "id", "requesttime", "approvetime", "id", "id" };
+
+	/*
+	 * Gets all users and their personal information, ordered by their type.
+	 * ('admin' > 'forseller' > 'seller' > 'deleted' > 'buyer')
+	 */
+	@GetMapping("user")
+	public List<Map<String, Object>> user() {
+		List<Map<String, Object>> userList = new ArrayList<Map<String, Object>>();
+		for (int i = 0; i < TYPE.length; ++i) {
+			userList.addAll(userRepository.getUsers(TYPE[i], ORDER_BY[i], "DESC"));
+		}
+		return userList;
+	}
+
+}
---
+++ backend/src/main/java/cn/edu/pku/course/database/idlefish/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package cn.edu.pku.course.database.idlefish.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	private static String basicInfo = "id, name, birth, sex, email, phone, type";
+
+	public List<Map<String, Object>> getUsers(String type, String orderBy, String order) {
+		String sql = "SELECT " + basicInfo;
+		if (type == "forseller") {
+			sql += ", requesttime FROM account INNER JOIN forseller ON id = forsellerid";
+		} else if (type == "seller") {
+			sql += ", approvetime FROM account INNER JOIN seller ON id = sellerid";
+		} else {
+			sql += " FROM account";
+		}
+		sql += " WHERE type = '" + type + "' ORDER BY " + orderBy + " " + order;
+		return jdbcTemplate.queryForList(sql);
+	}
+
+}
